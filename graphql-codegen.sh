e0364f0a480eac3a98d95e516e6a3570f37231e0
Steve Cheong <j.stephen.cheong@gmail.com>
2021-05-19 01:20:16 +0900

    Fixed endpoint, added forceful param

--- graphql-codegen.sh
+++ graphql-codegen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx get-graphql-schema http://dev.mealkit.lessbutter.co/query > lib/graphql/schema.graphql
-flutter pub run build_runner build
+npx get-graphql-schema https://mealkit.lessbutter.co/query > lib/graphql/schema.graphql
+flutter pub run build_runner build --delete-conflicting-outputs
